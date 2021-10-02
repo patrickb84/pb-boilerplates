aa83b117c6b71d69429820b7e0638c92c63b3a42
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2021-10-01 22:03:18 -0700

    Update Auth.js

--- react/start-react/src/store/Auth.js
+++ react/start-react/src/store/Auth.js
@@ -1,16 +1,19 @@
 import { UserCollection } from '../data';
+import { setUser } from '../providers/AppContextProvider';
 
-const Auth = {};
-
-Auth.Login = (email, password) => {
+export const login = (email, password) => {
   try {
     const user = UserCollection.find(u => u.email === email)[0];
 
     if (!user) throw new Error('No account found');
     if (user.password !== password) throw new Error('Password incorrect');
-    else return user;
-    
+    // else return user;
+    setUser(user);
   } catch (error) {
     console.error(error);
   }
+};
+
+export const logOut = () => {
+  setUser(null);
 };
